ae2ed76965713791357a22d806ab8b901917e635
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2024-11-14 14:29:35 -0800

    stuff

--- src/example.cpp
+++ src/example.cpp
@@ -3,6 +3,10 @@
 #include <lib/dawn_wrapper/src/dawn_wrapper.h>
 #include <lib/glfw_wrapper/src/glfw_wrapper.h>
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten/html5.h>
+#endif
+
 int main()
 {
     using namespace std;
@@ -11,25 +15,36 @@ int main()
 
   auto c = dawn.make_compute();
 
-  glfw_wrapper::init_glfw(std::cout);
-
   auto tick_mark = std::chrono::system_clock::now();
 
+#ifndef __EMSCRIPTEN__
+  glfw_wrapper::init_glfw(std::cout);
   auto window = glfw_wrapper::Window::make_window(1200, 800, false, true, "wgui");
 
   while (true) {
-
       auto next_tick_mark = std::chrono::system_clock::now();
       chrono::duration<float_t> diff(next_tick_mark - tick_mark);
+      glfw_wrapper::poll_events();
+
+      tick_mark = next_tick_mark;
+
+      std::cout << diff.count() << std::endl;
+  }
+  glfw_wrapper::terminate();
+#else
+    emscripten_set_main_loop_arg(
+        [](void* userData) {
+            std::cout << "a " << "\n";
+        },
+        nullptr,
+        0, true);
+#endif
+
      // if (!instance.update(std::min(0.05f, diff.count()))) {
      // instance.release();
      //   break;
      // }
      //
-      glfw_wrapper::poll_events();
-      tick_mark = next_tick_mark;
-  }
 
-  glfw_wrapper::terminate();
   return 0;
 }
