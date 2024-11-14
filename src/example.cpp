47ff5aea78c545eed3cc4ba0ff6be4ddbf407a9a
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2024-11-14 15:16:25 -0800

    working on rendering to html canvas

--- src/example.cpp
+++ src/example.cpp
@@ -7,36 +7,62 @@
 #include <emscripten/html5.h>
 #endif
 
+#include <cassert>
+#define ASSERT(p) cassert((p))
+
+#ifndef __EMSCRIPTEN__
+static void setup_surface(dawn_wrapper::render_wrapper renderer, glfw_wrapper::Window window, bool opaque = true)
+{
+    ASSERT(renderer);
+    int w = 0, h = 0;
+    window.get_window_size(w, h);
+    renderer.setup_surface(window.impl(), w, h, opaque);
+}
+#endif
+
+struct States {
+    dawn_wrapper::dawn_plugin m_dawn;
+    bool m_is_done = false;
+};
+
 int main()
 {
     using namespace std;
 
-    dawn_wrapper::dawn_plugin dawn;
-
-  auto c = dawn.make_compute();
-
-  auto tick_mark = std::chrono::system_clock::now();
+    auto tick_mark = std::chrono::system_clock::now();
 
 #ifndef __EMSCRIPTEN__
-  glfw_wrapper::init_glfw(std::cout);
-  auto window = glfw_wrapper::Window::make_window(1200, 800, false, true, "wgui");
+    dawn_wrapper::dawn_plugin dawn;
+    glfw_wrapper::init_glfw(std::cout);
+    auto window = glfw_wrapper::Window::make_window(1200, 800, false, true, "wgui");
 
-  while (true) {
-      auto next_tick_mark = std::chrono::system_clock::now();
-      chrono::duration<float_t> diff(next_tick_mark - tick_mark);
-      glfw_wrapper::poll_events();
+    auto renderer = dawn.make_render();
 
-      tick_mark = next_tick_mark;
+    setup_surface(renderer, window, false);
 
-      std::cout << diff.count() << std::endl;
-  }
-  glfw_wrapper::terminate();
+    while (true) {
+        auto next_tick_mark = std::chrono::system_clock::now();
+        chrono::duration<float_t> diff(next_tick_mark - tick_mark);
+        glfw_wrapper::poll_events();
+
+        tick_mark = next_tick_mark;
+
+        std::cout << diff.count() << std::endl;
+    }
+    glfw_wrapper::terminate();
 #else
+    States states;
     emscripten_set_main_loop_arg(
         [](void* userData) {
-            std::cout << "a " << "\n";
+            States * states = reinterpret_cast<States *>(userData);
+            if (states->m_dawn && !states->m_is_done) {
+                states->m_is_done = true;
+                auto renderer = states->m_dawn.make_render();
+
+                renderer.setup_surface_html_canvas("#canvas", 500, 500);
+            }
         },
-        nullptr,
+        & states,
         0, true);
 #endif
 
